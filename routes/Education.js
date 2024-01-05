04fd16386c851252f38d65a07b8279ee56f4782b
Steven Chen <chensteven0617@gmail.com>
2024-01-04 16:37:58 -0800

    changed cases

--- routes/Education.js
+++ routes/Education.js
@@ -5,14 +5,14 @@ const path = require("path");
 const matter = require("gray-matter");
 
 const eduPath = path.join(__dirname, "../education");
-router.get("/", (req, res) => {
+router.get("", (req, res) => {
   try {
     const filePath = path.join(eduPath || process.env.EDU_PATH, "education.md");
     const fileContent = fs.readFileSync(filePath, "utf8");
     const { data, content } = matter(fileContent);
     res.json({ frontMatter: data, content: content });
   } catch (error) {
-    console.error("Error reading project file:", error);
+    console.error("Error reading education file:", error);
     res.status(500).send("Internal Server Error");
   }
 });
