8dd047fb3d01c75534ffa5f0ec73ddf2835667fc
Steven Chen <chensteven0617@gmail.com>
2024-01-04 23:03:28 -0800

    deleted procfile

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ const educationRouter = require("./routes/Education");
 
 app.use("/projects", projectRouter);
 app.use("/education", educationRouter);
-app.use((req, res, next) => {
-  if (req.originalUrl && req.originalUrl.split("/").pop() === "favicon.ico") {
-    return res.sendStatus(204);
-  }
-  return next();
-});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is starting at ${PORT}`);
