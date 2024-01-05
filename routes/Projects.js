896da776133856ff87e0526c8f12bf1a36e0e441
Steven Chen <chensteven0617@gmail.com>
2024-01-04 16:53:37 -0800

    Update Projects.js

--- routes/Projects.js
+++ routes/Projects.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const matter = require("gray-matter");
 
-const projectsPath = path.join(__dirname, "../projects");
+const projectsPath = path.join(__dirname, "..", "projects");
 // Get all projects
 router.get("/:projectId", (req, res) => {
   const projectId = req.params.projectId;
   try {
-    const filePath = path.join(projectsPath || process.env.PROJECT_PATH, `${projectId}.md`);
+    const filePath = path.join(projectsPath, `${projectId}.md`);
     const fileContent = fs.readFileSync(filePath, "utf8");
     const { data, content } = matter(fileContent);
     res.json({ frontMatter: data, content: content });
